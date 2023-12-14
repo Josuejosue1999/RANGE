fe3269c0747ca017ba61472fd75a2dab486b5008
dkokonkwo <d.okonkwo@alustudent.com>
2023-12-14 00:11:47 -0400

    front and back updates

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -68,6 +68,17 @@ def update_task(task_id):
     return render_template('update.html', form=form, task=task)
 
 
+@app.route("/tasks/<int:task_id>/delete", methods=['GET', 'POST'])
+@login_required
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    if task.creator != current_user:
+        abort(403)
+    db.session.delete(task)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('tasks'))
+
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
